c86ee3ecb72db37990f19efbddca0f5369a6a911
Mitsul777 <131904370+Mitsul777@users.noreply.github.com>
2023-12-03 09:12:03 +0000

    Deploying to gh-pages from @ Mitsul777/goit-js-hw-09@19d478405f46c75c7dc4deaab13d6853c9ed7827 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{S as a}from"./assets/vendor-c9def49e.js";new a(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=commonHelpers.js.map
